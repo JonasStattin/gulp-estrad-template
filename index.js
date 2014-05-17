4157e0ebeb8720e3d0bed7800712e9e3919a5693
Per Stenström <per.stenstrom@gmail.com>
2014-05-17 16:40:41 +0200

    Update estrad-template version

--- index.js
+++ index.js
@@ -3,18 +3,33 @@
 		through = require('async-through'),
 		gutil = require('gulp-util'),
 		partials = require("estrad-template"),
+		extend = require("node-extend"),
 		PluginError = gutil.PluginError;
 
 	const PLUGIN_NAME = 'gulp-partials';
 
 	module.exports = Partials;
 
-	function Partials() {
-		return through(function(file){
-			if (file.isNull()) return;
-			var self = this;
+	function Partials(args) {
+		var 
+			defaults = {
+				folder: 'modules'
+			},
+			obj;
 
-			partials(file.contents, function(err, content){
+		if(typeof args === 'object') {
+			obj = extend(defaults, args);
+		} else {
+			obj = defaults;
+		}
+
+		return through(function(file){
+			var 
+				self = this;
+
+			if (file.isNull()) return;
+
+			partials(file.contents, obj, function(err, content){
 				file.contents = new Buffer(content);
 
 				self.emit('data', file);
